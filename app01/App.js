0d8eab2b9088693226196ed2e66f1d0fb2b1eec6
Brian2018 <17602193998@163.com>
2018-08-20 22:50:11 +0800

    gengxin

--- app01/App.js
+++ app01/App.js
@@ -3,56 +3,45 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+  View,
+  Image
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+import {
+  createStackNavigator,
+} from 'react-navigation';
+import Login from './component/login'
+class starScreen extends React.Component {  
+  static navigationOptions = () => ({
+    header:null, 
+  });
+  componentDidMount(){    
+    window.start=setTimeout(() => this.props.navigation.navigate('Login'),1000)
+  }
+  componentWillUnmount(){
+    window.clearTimeout(window.start);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image source={require('./public/img/start.png')} style={{ width: 150, height: 80 }} />         
       </View>
     );
   }
 }
+const RootStack = createStackNavigator(
+  {
+    Start: starScreen,
+    Login: Login,
+  },  
+  {
+    initialRouteName: 'Start',
+  }, 
+);
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
