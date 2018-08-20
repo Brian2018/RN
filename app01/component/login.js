2f01fa994b3fb5f0afb69d988d26cf624e05fbb3
Brian2018 <17602193998@163.com>
2018-08-20 22:50:39 +0800

    gengxin

---
+++ app01/component/login.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  Button
+} from 'react-native';
+import {
+  createStackNavigator,
+} from 'react-navigation';
+export default class App extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    header:null, 
+  });
+  render() {      
+    return (
+      <View style={styles.page}>
+        <Image source={require('../public/img/login/11.png')} style={styles.img1} />        
+        <Image source={require('../public/img/login/12.png')} style={styles.imgText}/>
+        <View style={{flexDirection:'row'}}>
+          <Image source={require('../public/img/login/07.png')} style={styles.img07}/>
+          <Text style={styles.text}>账号:</Text>
+          <TextInput style={styles.input} placeholder="请输入用户名"/>
+        </View>        
+        <View style={{flexDirection:'row'}}>
+          <Image source={require('../public/img/login/08.png')} style={styles.img07}/>
+          <Text style={styles.text}>密码:</Text>
+          <TextInput password={true} style={styles.input} placeholder="请输入密码"/>
+        </View> 
+        <View style={{margin:50}}>
+          <Button style={styles.button} title='登录'/>    
+        </View>
+        <Image source={require('../public/img/login/15.png')} style={{width:65,height:55,marginLeft:150 }}/>
+        <Image source={require('../public/img/login/18.png')} style={{width:360,height:70,marginLeft:0,position:'absolute',bottom:0}}/>
+      </View>
+    );
+  }
+}
+const styles=StyleSheet.create({
+  page:{
+    flex:1,
+    backgroundColor:'white',      
+  },
+  img1:{
+    width:120, 
+    height:120,
+    marginLeft:120,
+    marginTop:50
+  },
+  img07:{
+    width:25,
+    height:25,
+    marginTop:30,
+    marginLeft:50
+  },
+  imgText:{
+    width:120,
+    height:15,
+    marginLeft:120,
+    marginTop:10
+  },
+  input:{
+    width:200,
+    height: 40,
+    marginTop: 20,    
+    fontSize:17,    
+  },
+  text:{
+    marginTop:30,
+    padding:5
+  },
+  button:{
+    width:200,
+    marginLeft:20,
+  }
+})
+
+
